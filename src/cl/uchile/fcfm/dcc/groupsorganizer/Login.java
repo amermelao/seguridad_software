8f93c56ae12841d2135098919561ef5c620de4d2
Gonzalo Infante <chalo.infante01@gmail.com>
2014-04-13 12:54:47 -0300

    Initial import.

---
+++ src/cl/uchile/fcfm/dcc/groupsorganizer/Login.java
@@ -0,0 +1,15 @@
+package cl.uchile.fcfm.dcc.groupsorganizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
